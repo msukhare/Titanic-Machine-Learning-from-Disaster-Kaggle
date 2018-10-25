e02a3fcaf15f5ccd4be25e9709ef1825031b4746
Mark SUKHAREV <msukhare@e3r6p8.42.fr>
2018-10-25 17:31:05 +0200

    tests

--- read_and_complete_data.py
+++ read_and_complete_data.py
@@ -6,13 +6,14 @@
 #    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/22 16:33:56 by msukhare          #+#    #+#              #
-#    Updated: 2018/10/23 18:04:43 by kemar            ###   ########.fr        #
+#    Updated: 2018/10/25 17:30:38 by msukhare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
 import pandas as pd
 import sys
+import time
 
 def create_feat_size_family(sibsp, parch):
     new_feat = np.zeros((sibsp.shape[0], 1), dtype=int)
@@ -20,16 +21,9 @@ def create_feat_size_family(sibsp, parch):
         new_feat[i][0] = sibsp[i] + parch[i]
     return (new_feat)
 
-def if_child(data):
-    return (0)
-
 def change_Age_into_bool(data):
     new_feat = np.zeros((data.shape[0], 1), dtype=int)
     for i in range(int(data.shape[0])):
-        """
-        if (pd.isna(data['Age'][i])):
-            new_feat[i][0] = if_child(data)
-            """
         if (data['Age'][i] < 17):
             new_feat[i][0] = 1
     return (new_feat)
@@ -37,52 +31,67 @@ def change_Age_into_bool(data):
 def categorize_age(age):
     new_feat = np.zeros((age.shape[0], 1), dtype=int)
     for i in range(int(age.shape[0])):
-        if (age[i] >= 0 and age[i] <= 16):
+        if (age[i] <= 16):
             new_feat[i][0] = 1
-        elif (age[i] > 16 and age[i] <= 40):
+        """
+        if (age[i] >= 0 and age[i] <= 8):
+            new_feat[i][0] = 1
+        elif (age[i] > 8 and age[i] <= 12):
             new_feat[i][0] = 2
-        else:
+        elif (age[i] > 12 and age[i] <= 16):
             new_feat[i][0] = 3
+        elif (age[i] > 16 and age[i] <= 32):
+            new_feat[i][0] = 4
+        elif (age[i] > 32 and age[i] <= 36):
+            new_feat[i][0] = 5
+        elif (age[i] > 36 and age[i] <= 48):
+            new_feat[i][0] = 6
+        elif (age[i] > 48 and age[i] <= 52):
+            new_feat[i][0] = 7
+        elif (age[i] > 52 and age[i] <= 64):
+            new_feat[i][0] = 8
+        else:
+            new_feat[i][0] = 9
+            """
     return (new_feat)
 
-def check_if_same_data(name, ticket, sibsp, sex, pos):
-    for i in range(name.shape[0]):
-        if (i != pos and ticket[i] == ticket[pos] and sibsp[i] > 0):
-            if (sex[pos] == 1):
-                last_name_find = name[pos].split(",")
-                last_name_find = last_name_find[1]
-            else:
-                last_name_find = name[pos].split("(")
-                last_name_find = last_name_find[0].split(",")
-                last_name_find = last_name_find[1]
-            if (sex[i] == 1):
-                last_name = name[i].split(",")
-                last_name = last_name[1]
-            else:
-                last_name = name[i].split("(")
-                last_name = last_name[0].split(",")
-                last_name = last_name[1]
-            if (last_name == last_name_find):
-                return (1)
+def create_dummy_feat(embarked, letter):
+    new_feat = np.zeros((embarked.shape[0], 1), dtype=int)
+    for i in range(embarked.shape[0]):
+        if (embarked[i] == letter):
+            new_feat[i][0] = 1
+    return (new_feat)
+
+def get_person_who_is_married(data, pos):
+    if (data['Name'][pos].find("Master") != -1 or data['Name'][pos].find("Miss") != -1):
+        print(data['Name'][pos])
+        return (-1)
+    tmp = data['Name'][pos].split(",")
+    
     return (0)
 
-def create_married(data):
+def get_if_married(data):
     new_feat = np.zeros((data.shape[0], 1), dtype=int)
     for i in range(data.shape[0]):
-        if (data['SibSp'][i] > 0 and check_if_same_data(data['Name'], data['Ticket'],\
-                data['SibSp'], data['Sex'], i)):
-            new_feat[i][0] = 1
+        if (data['SibSp'][i] > 0 and new_feat[i][0] == 0):
+            sec_person = get_person_who_is_married(data, i)
+            if (sec_person >= 0):
+                new_feat[i][0] = 1
+                new_feat[sec_person][0] = 1
     return (new_feat)
 
 def complete_data(data):
     data['Sex'] = data['Sex'].map({'male' : 1, 'female': 0})
     data['Embarked'] = data['Embarked'].map({'S' : 1, 'C': 2, 'Q': 3})
-    data.fillna(value={'Age': data['Age'].quantile(0.50)}, inplace=True)
+    data.fillna(value={'Age': data['Age'].mean()}, inplace=True)
     data.fillna(value={'Embarked': data['Embarked'].quantile(0.50)}, inplace=True)
-    data.fillna(value={'Fare': data['Fare'].quantile(0.50)}, inplace=True)
-    data.insert(data.shape[1], 'Child_or_not', change_Age_into_bool(data))
-    #data.insert(data.shape[1], 'Age_category', categorize_age(data['Age']))
-    data.insert(data.shape[1], 'Married', create_married(data))
+    data.fillna(value={'Fare': data['Fare'].mean()}, inplace=True)
+    #data.insert(data.shape[1], 'Child_or_not', change_Age_into_bool(data))
+   # data.insert(data.shape[1], 'C', create_dummy_feat(data['Embarked'], 2))
+   # data.insert(data.shape[1], 'S', create_dummy_feat(data['Embarked'], 1))
+   # data.insert(data.shape[1], 'Q', create_dummy_feat(data['Embarked'], 3))
+    data.insert(data.shape[1], 'Age_categorize', categorize_age(data['Age']))
+    data.insert(data.shape[1], 'Married', get_if_married(data))
     data.insert(data.shape[1], 'Size_family', \
             create_feat_size_family(data['SibSp'], data['Parch']))
     data = data.drop(['Name', 'Ticket', 'Cabin', 'Parch', 'SibSp', 'Age'], axis=1)
--- train_rand_tree.py
+++ train_rand_tree.py
@@ -6,12 +6,13 @@
 #    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/22 16:14:31 by msukhare          #+#    #+#              #
-#    Updated: 2018/10/23 14:35:28 by kemar            ###   ########.fr        #
+#    Updated: 2018/10/25 17:30:39 by msukhare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import pandas as pd
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
 from describe_data_train import read_file
 from sklearn.ensemble import RandomForestClassifier
@@ -22,16 +23,23 @@ def main():
     try:
         Y_test = pd.read_csv("gender_submission.csv")
     except:
-        sys.exi("fail to open file")
+        sys.exit("fail to open file")
     Y_test = Y_test.iloc[:, 1:]
     Y_test = np.array(Y_test.values, dtype=int)
     X, Y, X_test, passager_id = read_file(0)
     Y = np.reshape(Y, (Y.shape[0]))
-    for i in range(1, 100):
-        random_forest = RandomForestClassifier(n_estimators=123, random_state=i)
+    error_rate = []
+    nb_tree = []
+    for i in range(1, 100): #40
+        random_forest = RandomForestClassifier(n_estimators=i, random_state=97)
         random_forest.fit(X, Y)
+        y_pred_train = random_forest.predict(X)
+        error_rate.append(1 - accuracy_score(Y, y_pred_train))
+        nb_tree.append(i)
         y_pred_test = random_forest.predict(X_test)
         print(accuracy_score(Y_test, y_pred_test))
+    plt.plot(nb_tree, error_rate)
+    plt.show()
 
 if (__name__ == "__main__"):
     main()
